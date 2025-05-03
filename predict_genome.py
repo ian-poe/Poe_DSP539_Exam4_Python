931d5a9c9a75e22d3821827102a5e286646378f1
Ian Poe <ian_poe@uri.edu>
2025-05-03 18:02:50 -0400

    Code from Notebook Translated to py format (File Output)

--- predict_genome.py
+++ predict_genome.py
@@ -18,10 +18,20 @@ def predicted_genome(s, k):
 # Get command-line inputs
 file_path = sys.argv[1]  # Input 1: File name
 k = int(sys.argv[2])     # Input 2: Convert to integer
+output_file_name = sys.argv[3] #Input 3: Output File Name
 
-# Read the file and clean the input
+# Read data as a single string
 with open(file_path, "r") as file:
-    file_content = file.read().strip()  # Removes extra whitespace/newlines
+    file_content = file.read().strip()  # remove any spaceing between lines
 
 # Run the function and print results
-print(predicted_genome(file_content, k))
+#print(predicted_genome(file_content, k))
+
+ #OUTPUT of the Function
+output = predicted_genome(file_content, k) 
+#Export New Text File
+with open(output_file_name, "w") as out_file: #Open a Text File
+    for key, value in output.items():
+        out_file.write("{}: {}\n".format(key, value)) #Format for the new File
+
+print("Output saved to {}".format(output_file_name))  #Print Conformation for File Output
