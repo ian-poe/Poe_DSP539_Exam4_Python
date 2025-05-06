bf19c56169b75cec7410499ba8d96d3432bd9b75
Ian Poe <ian_poe@uri.edu>
2025-05-06 14:48:07 -0400

    Included More Cases

--- exam4_test.py
+++ exam4_test.py
@@ -5,6 +5,9 @@ from predict_genome import (read_genome_file, identify_genomes, count_total_geno
 # Sample genome sequence for testing
 sample_genome_1 = "ATGTCTGTCTGAA"  #Length 13
 sample_genome_2 = "ATGTCTGTCTGAACTGTCT" #Length 19
+sample_genome_3 = "A"
+sample_genome_4 = "AT" 
+sample_genome_5 = "ATG" 
 k_2 = 2
 k_3 = 3
 def test_identify_genomes():
@@ -13,34 +16,51 @@ def test_identify_genomes():
     expected_3_2 = ["ATG", "TGT", "GTC", "TCT", "CTG", "TGT", "GTC", "TCT", "CTG", "TGA", "GAA", "AAC", "ACT", "CTG", "TGT", "GTC", "TCT"]
     expected_2_1 = ["AT", "TG", "GT", "TC", "CT", "TG", "GT", "TC", "CT", "TG", "GA", "AA"]
     expected_2_2 = ["AT", "TG", "GT", "TC", "CT", "TG", "GT", "TC", "CT", "TG", "GA", "AA", "AC", "CT", "TG", "GT", "TC", "CT"]
+    expected_3 = []
+    expected_4 = ["AT"]
+    expected_5 = ["AT", "TG"]
     assert identify_genomes(sample_genome_1, k_2) == expected_2_1
     assert identify_genomes(sample_genome_1, k_3) == expected_3_1   
     assert identify_genomes(sample_genome_2, k_2) == expected_2_2
     assert identify_genomes(sample_genome_2, k_3) == expected_3_2
-        
+    assert identify_genomes(sample_genome_3, k_3) == expected_3
+    assert identify_genomes(sample_genome_4, k_2) == expected_4
+    assert identify_genomes(sample_genome_5, k_2) == expected_5
+    
 def test_count_total_genomes():
     """Test count_total_genomes function."""
     expected_2_1 = {"AT": 1, "TG": 3, "GT": 2, "TC": 2, "CT": 2, "GA": 1, "AA": 1}
     expected_3_1 = {"ATG": 1, "TGT": 2, "GTC": 2, "TCT": 2, "CTG": 2, "TGA": 1, "GAA": 1}
     expected_2_2 = {"AT": 1, "TG": 4, "GT": 3, "TC": 3, "CT": 4, "GA": 1, "AA": 1, "AC": 1}
     expected_3_2 = {"ATG": 1, "TGT": 3, "GTC": 3, "TCT": 3, "CTG": 3, "TGA": 1, "GAA": 1, "AAC": 1, "ACT": 1}
-
+    expected_3 = {}
+    expected_4 = {"AT": 1}
+    expected_5 = {"AT": 1, "TG": 1}
     assert count_total_genomes(sample_genome_1, k_2) == expected_2_1
     assert count_total_genomes(sample_genome_1, k_3) == expected_3_1
     assert count_total_genomes(sample_genome_2, k_2) == expected_2_2
     assert count_total_genomes(sample_genome_2, k_3) == expected_3_2
-
+    assert count_total_genomes(sample_genome_3, k_3) == expected_3
+    assert count_total_genomes(sample_genome_4, k_2) == expected_4
+    assert count_total_genomes(sample_genome_5, k_2) == expected_5
+    
 def test_next_genomes():
     """Test next_genomes function."""
     expected_2_1 = ["TG", "GT", "TC", "CT", "TG", "GT", "TC", "CT", "TG", "GA", "AA"]
     expected_3_1 = ["TGT", "GTC", "TCT", "CTG", "TGT", "GTC", "TCT", "CTG", "TGA", "GAA"]
     expected_2_2 = ["TG", "GT", "TC", "CT", "TG", "GT", "TC", "CT", "TG", "GA", "AA", "AC", "CT", "TG", "GT", "TC", "CT"]
     expected_3_2 = ["TGT", "GTC", "TCT", "CTG", "TGT", "GTC", "TCT", "CTG", "TGA", "GAA", "AAC", "ACT", "CTG", "TGT", "GTC", "TCT"]
+    expected_3 = []
+    expected_4 = []
+    expected_5 = ["TG"]
     assert next_genomes(sample_genome_1, k_2) == expected_2_1
     assert next_genomes(sample_genome_1, k_3) == expected_3_1
     assert next_genomes(sample_genome_2, k_2) == expected_2_2
     assert next_genomes(sample_genome_2, k_3) == expected_3_2
-
+    assert next_genomes(sample_genome_3, k_3) == expected_3
+    assert next_genomes(sample_genome_4, k_2) == expected_4
+    assert next_genomes(sample_genome_5, k_2) == expected_5
+    
 def test_count_next_genomes():
     """Test count_next_genomes function."""
     expected_3_1 = {
@@ -76,12 +96,18 @@ def test_count_next_genomes():
      'GA': {'AA': 1},
      'AA': {'AC': 1},
      'AC': {'CT': 1}}
+    expected_3 = {}
+    expected_4 = {}
+    expected_5 = {'AT': {'TG': 1}}
     
     assert count_next_genomes(sample_genome_1, k_2)[0] == expected_2_1
     assert count_next_genomes(sample_genome_1, k_3)[0] == expected_3_1
     assert count_next_genomes(sample_genome_2, k_2)[0] == expected_2_2
     assert count_next_genomes(sample_genome_2, k_3)[0] == expected_3_2
-
+    assert count_next_genomes(sample_genome_3, k_3)[0] == expected_3
+    assert count_next_genomes(sample_genome_4, k_2)[0] == expected_4
+    assert count_next_genomes(sample_genome_5, k_2)[0] == expected_5
+    
 def test_check_edge_values():
     """Test check_edge_values function."""
     expected_2_1 = "AA"
